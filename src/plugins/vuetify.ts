f366768ad246fef084e3b59e63bf5f387b405d70
devgaram <joingaram@gmail.com>
2020-03-05 21:58:41 +0900

    Update vuetify theme

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -3,4 +3,15 @@ import Vuetify from "vuetify/lib";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  icons: {
+    iconfont: "mdiSvg"
+  },
+  theme: {
+    themes: {
+      light: {
+        primary: "#1ec0ff"
+      }
+    }
+  }
+});
