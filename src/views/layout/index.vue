5a645e52b176a6c3e68beec6701b1d3725ef4019
devgaram <joingaram@gmail.com>
2020-03-05 22:02:39 +0900

    Implement layout views

---
+++ src/views/layout/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <Navigation />
+    <Header />
+    <v-content><slot /></v-content>
+    <Footer />
+  </v-app>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import Header from "@/components/layout/header/index.vue";
+import Footer from "@/components/layout/footer/index.vue";
+import Navigation from "@/components/layout/navigation/index.vue";
+@Component({
+  components: {
+    Navigation,
+    Header,
+    Footer
+  }
+})
+export default class Layout extends Vue {}
+</script>
