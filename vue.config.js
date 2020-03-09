14d38e0ffdb9dde8c1fffc6a186c7e6416031384
Seung Hyeon Kim (James) <tmdgus0118@gmail.com>
2020-03-09 23:33:42 +0900

    Update Jenkinsfile to change base_url

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
+let jenkins = {}
+try {
+  jenkins = require('./_jenkins.json')
+} finally {}
+
 module.exports = {
   transpileDependencies: ["vuetify", "vuex-module-decorators"],
-  publicPath: ""
+  publicPath: jenkins.publicPath || ''
 };
