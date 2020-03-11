4522dc678edac481de203eb345990c3ae7262417
devgaram <joingaram@gmail.com>
2020-03-12 00:20:38 +0900

    Modify vuetify theme & icon

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,22 +1,26 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
+import "@mdi/font/css/materialdesignicons.css";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
   icons: {
-    iconfont: "mdiSvg"
+    iconfont: "mdi"
   },
   theme: {
     themes: {
       light: {
-        primary: "#039BE5",
-        secondary: "#424242",
-        accent: "#80D8FF",
+        primary: "#8D13D0",
+        secondary: "#000000",
+        accent: "#E040FB",
         error: "#FF5252",
         info: "#03A9F4",
         success: "#4CAF50",
         warning: "#FFC107",
+      },
+      dark: {
+        primary: "#8D13D0"
       }
     }
   }
