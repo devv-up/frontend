10c22d12fd17250b9ed40b75f8abaa9c541449d4
devgaram <joingaram@gmail.com>
2020-04-08 21:02:18 +0900

    Add lazy component

--- src/components/CardView/index.vue
+++ src/components/CardView/index.vue
@@ -1,22 +1,30 @@
 <template>
-  <v-card max-width="280" outlined class="ma-auto">
-    <v-img
-      class="white--text align-end"
-      max-height="200"
-      :src="item.images && item.images[0]"
-    >
-      <v-card-title>{{ item.title }}</v-card-title>
-    </v-img>
-    <v-row>
-      <v-col>
-        <v-icon class="mx-2">mdi-heart</v-icon>
-        <v-icon>mdi-share-variant</v-icon>
-      </v-col>
-      <v-col class="text-right">
-        <span class="mr-2">{{ item.date }}</span>
-      </v-col>
-    </v-row>
-  </v-card>
+  <v-lazy
+    v-model="isActive"
+    :options="{
+      threshold: 0.5
+    }"
+    transition="fade-transition"
+  >
+    <v-card max-width="280" outlined class="ma-auto">
+      <v-img
+        class="white--text align-end"
+        max-height="200"
+        :src="item.images && item.images[0]"
+      >
+        <v-card-title>{{ item.title }}</v-card-title>
+      </v-img>
+      <v-row>
+        <v-col>
+          <v-icon class="mx-2">mdi-heart</v-icon>
+          <v-icon>mdi-share-variant</v-icon>
+        </v-col>
+        <v-col class="text-right">
+          <span class="mr-2">{{ item.date }}</span>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-lazy>
 </template>
 
 <script lang="tsx">
@@ -27,7 +35,9 @@ import Component from "vue-class-component";
     item: Object
   }
 })
-export default class CardView extends Vue {}
+export default class CardView extends Vue {
+  isActive = false;
+}
 </script>
 <style lang="scss" scoped>
 .v-card__title {
