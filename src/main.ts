788563f49536504c01fc28c8fd07fe961e90a46d
yimsb01 <yimsb02@naver.com>
2020-06-26 14:31:00 +0900

    Set vue to use Infinite Loading

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,14 @@ import store from "@/store";
 import vuetify from "@/plugins/vuetify";
 import ResizeComponent from "@/components/common/ResizeComponent.vue";
 import ModalComponent from "@/components/common/ModalComponent.vue";
+import InfiniteLoading from "vue-infinite-loading";
 
 Vue.config.productionTip = false;
+
 Vue.component("v-resize", ResizeComponent);
 Vue.component("v-modal", ModalComponent);
+
+Vue.use(InfiniteLoading);
 
 new Vue({
   router,
