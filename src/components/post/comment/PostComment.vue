f87cfaffaa7ce35c0c88f2094636278a162b7ffd
dbzero4143 <42237836+dbzero4143@users.noreply.github.com>
2020-06-27 15:50:04 +0900

    [feature/comment] modifying
    [feature/comment] modifying

--- src/components/post/comment/PostComment.vue
+++ src/components/post/comment/PostComment.vue
@@ -8,15 +8,29 @@
         </h3>
         <v-spacer />
         <v-btn class="ma-1" color="white" @click="update(item.id)">수정</v-btn>
-        <v-btn class="ma-1 white--text" color="#8d13d0" @click="deletecomment(item.id)">삭제</v-btn>
+        <v-btn
+          class="ma-1 white--text"
+          color="#8d13d0"
+          @click="deletecomment(item.id)"
+          >삭제</v-btn
+        >
       </v-row>
       <p name="content" v-if="item.id">{{ item.content }}</p>
     </span>
 
     <p class="font-weight-CONDENSED headline ma-2">댓글쓰기</p>
     <span align="center">
-      <v-textarea name="input-7-1" filled label="댓글입력" auto-grow class="mt-8" v-model="comment"></v-textarea>
-      <v-btn class="white--text" color="#8d13d0" @click="addcomment">댓글등록</v-btn>
+      <v-textarea
+        name="input-7-1"
+        filled
+        label="댓글입력"
+        auto-grow
+        class="mt-8"
+        v-model="comment"
+      ></v-textarea>
+      <v-btn class="white--text" color="#8d13d0" @click="addcomment"
+        >댓글등록</v-btn
+      >
     </span>
   </section>
 </template>
