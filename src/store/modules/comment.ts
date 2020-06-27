417ed621be3cdeeb09466f838877a56c31509328
dbzero4143 <42237836+dbzero4143@users.noreply.github.com>
2020-06-27 19:10:39 +0900

    [feature/comment] modifying
    [feature/comment] modifying

--- src/store/modules/comment.ts
+++ src/store/modules/comment.ts
@@ -2,48 +2,48 @@ import { Module, VuexModule, Mutation, Action } from "vuex-module-decorators";
 import { apiAddComment } from "@/utils/api/comment";
 import { apiDeleteComment } from "@/utils/api/comment";
 import { apiUpdateComment } from "@/utils/api/comment";
-import { Comment } from "@/store/models/Comment";
+import { Comment } from "@/store/models/comment";
 
 @Module
 export default class CommentModule extends VuexModule {
-	//State
-	private commentData!: Comment;
-	private commentUpdateData!: Record<string, string | number>;
+  //State
+  private commentData!: Comment;
+  private commentUpdateData!: Record<string, string | number>;
 
-	//Mutation
-	//댓글등록 뮤테
-	@Mutation
-	private commentAddMuta(mutacomment: Comment) {
-		this.commentData = mutacomment;
-	}
-	//댓글수정 뮤테
-	@Mutation
-	private commentUpdateMuta(mutacomment: Record<string, string | number>) {
-		this.commentUpdateData = mutacomment;
-	}
+  //Mutation
+  //댓글등록 뮤테
+  @Mutation
+  private commentAddMuta(mutacomment: Comment) {
+    this.commentData = mutacomment;
+  }
+  //댓글수정 뮤테
+  @Mutation
+  private commentUpdateMuta(mutacomment: Record<string, string | number>) {
+    this.commentUpdateData = mutacomment;
+  }
 
-	//Action
-	//댓글등록 액션
-	@Action({ commit: "commentAddMuta" })
-	async commentAddAction(actioncomment: Record<string, string | number>) {
-		return await apiAddComment(actioncomment);
-	}
-	//댓글수정 액션
-	@Action({ commit: "commentUpdateMuta" })
-	async commentUpdateAction(actioncomment: Record<string, string | number>) {
-		await apiUpdateComment(actioncomment);
-	}
-	//댓글삭제 액션
-	@Action
-	async commentDeleteAction(actioncommentid: Record<number, number | string>) {
-		return await apiDeleteComment(actioncommentid);
-	}
+  //Action
+  //댓글등록 액션
+  @Action({ commit: "commentAddMuta" })
+  async commentAddAction(actioncomment: Record<string, string | number>) {
+    return await apiAddComment(actioncomment);
+  }
+  //댓글수정 액션
+  @Action({ commit: "commentUpdateMuta" })
+  async commentUpdateAction(actioncomment: Record<string, string | number>) {
+    await apiUpdateComment(actioncomment);
+  }
+  //댓글삭제 액션
+  @Action
+  async commentDeleteAction(actioncommentid: Record<number, number | string>) {
+    return await apiDeleteComment(actioncommentid);
+  }
 
-	//getters
-	get gettercomment(): Comment {
-		return this.commentData;
-	}
-	get getterupdatecomment(): Record<string, string | number> {
-		return this.commentUpdateData;
-	}
+  //getters
+  get gettercomment(): Comment {
+    return this.commentData;
+  }
+  get getterupdatecomment(): Record<string, string | number> {
+    return this.commentUpdateData;
+  }
 }
--- src/utils/api/comment.ts
+++ src/utils/api/comment.ts
@@ -1,31 +1,31 @@
 import { http } from "@/utils/request";
-import { Comment } from "@/store/models/Comment";
+import { Comment } from "@/store/models/comment";
 
 /**
  * Get the list of postsssss
  *
  */
 export const apiAddComment = (params: Record<string, string | number>) => {
-	http.request<Comment>({
-		url: "/posts/comments",
-		method: "POST",
-		data: params,
-		withCredentials: true,
-		headers: { "Access-Control-Allow-Credentials": true }
-	});
+  http.request<Comment>({
+    url: "/posts/comments",
+    method: "POST",
+    data: params,
+    withCredentials: true,
+    headers: { "Access-Control-Allow-Credentials": true }
+  });
 };
 
 export const apiUpdateComment = (params: Record<string, string | number>) => {
-	http.request<Comment>({
-		url: `/posts/comments/${params}`,
-		method: "PUT",
-		data: params
-	});
+  http.request<Comment>({
+    url: `/posts/comments/${params}`,
+    method: "PUT",
+    data: params
+  });
 };
 
 export const apiDeleteComment = (params: Record<number, number | string>) => {
-	http.request({
-		url: `/posts/comments/${params}`,
-		method: "DELETE"
-	});
+  http.request({
+    url: `/posts/comments/${params}`,
+    method: "DELETE"
+  });
 };
